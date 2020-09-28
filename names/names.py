f6dee1906b5d0ae61bbe6c05a7c4e4956e6caee0
Elijah Atkins <elijahorama@gmail.com>
2020-09-27 21:03:23 -0600

    stretch done

--- names/names.py
+++ names/names.py
@@ -22,21 +22,44 @@ duplicates = []  # Return the list of duplicates in this data structure
 
 #imported binary tree from previous data project
 #creating a new node named root with first item in names_1
-root = BSTNode(names_1[0])
-#insert all names into root node
-for i in range(1, (len(names_1) - 1), 1):
-    root.insert(names_1[i])
+# root = BSTNode(names_1[0])
+# #insert all names into root node
+# for i in range(1, (len(names_1) - 1), 1):
+#     root.insert(names_1[i])
 
-#compare names_1 and names_2
-for name in names_2:
-    if root.contains(name):
-        duplicates.append(name)
-
-end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
+# #compare names_1 and names_2
+# for name in names_2:
+#     if root.contains(name):
+#         duplicates.append(name)
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
 # What's the best time you can accomplish?  Thare are no restrictions on techniques or data
 # structures, but you may not import any additional libraries that you did not write yourself.
+seen = set()
+#add all names in names_1 to a set
+for name in names_1:
+    seen.add(name)
+
+#compare all the names in names_2 with the set with all of names_1
+for name in names_2:
+    if name in seen:
+        duplicates.append(name)
+
+
+end_time = time.time()
+print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print (f"runtime: {end_time - start_time} seconds")
+
+
+#find all duplicates between list and within lists
+"""
+seen = set()
+names_3 = names_1 + names_2
+for name in names_3:
+    if name in seen:
+        if seen not in duplicates:
+            duplicates.append(name)
+    if name not in seen:
+        seen.add(name)
+"""
