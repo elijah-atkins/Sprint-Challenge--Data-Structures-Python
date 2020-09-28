f3c6eb4d863011dd93e182b9975697f2477f3c11
Elijah Atkins <elijahorama@gmail.com>
2020-09-27 21:09:44 -0600

    test done

--- names/names.py
+++ names/names.py
@@ -14,28 +14,32 @@ f.close()
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
-
+#runtime: 5.643412828445435 seconds
 # for name_1 in names_1:
 #     for name_2 in names_2:
 #         if name_1 == name_2:
 #             duplicates.append(name_1)
 
-#imported binary tree from previous data project
-#creating a new node named root with first item in names_1
-# root = BSTNode(names_1[0])
-# #insert all names into root node
-# for i in range(1, (len(names_1) - 1), 1):
-#     root.insert(names_1[i])
+# imported binary tree from previous data project
+# creating a new node named root with first item in names_1
+#runtime: 0.08428812026977539 seconds
+"""
+root = BSTNode(names_1[0])
+#insert all names into root node
+for i in range(1, (len(names_1) - 1), 1):
+    root.insert(names_1[i])
 
-# #compare names_1 and names_2
-# for name in names_2:
-#     if root.contains(name):
-#         duplicates.append(name)
-
+#compare names_1 and names_2
+for name in names_2:
+    if root.contains(name):
+        duplicates.append(name)
+"""
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
 # What's the best time you can accomplish?  Thare are no restrictions on techniques or data
 # structures, but you may not import any additional libraries that you did not write yourself.
+#runtime: 0.005338907241821289 seconds
+
 seen = set()
 #add all names in names_1 to a set
 for name in names_1:
