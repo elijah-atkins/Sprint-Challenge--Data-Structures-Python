5779552e28c527c88ba273529baa06ce8f502d65
Elijah Atkins <elijahorama@gmail.com>
2020-09-27 19:40:04 -0600

    testing

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -8,8 +8,33 @@ class RingBuffer:
         #define id and set as 0 
         self.id = 0
 
+    #Helper function to iterate id values
+    def iterate_id(self):
+        #using id values 0 to capacity minus 1
+        #resest id to 0 if capacity is reached
+        if self.id >= self.capacity - 1:
+            self.id = 0
+        #or add one to id
+        else:
+            self.id += 1
+
+    #function to append items
     def append(self, item):
-        pass
+        #check length of array to capacity if 
+        #array is full over write the item with 
+        #the current id
+        if len(self.buffer) == self.capacity:
+            #replace item with current id
+            self.buffer[self.id] = item
+            #jump to next id
+            self.iterate_id()
+            #if array is not full add to buffer
+        else:
+            self.buffer.append(item)
+            #iterate to next id
+            self.iterate_id()
+
 
     def get(self):
-        pass
+        return self.buffer
+            
