9b66923bb8fad31c076aedf759612a9884e36738
Elijah Atkins <elijahorama@gmail.com>
2020-09-27 19:07:00 -0600

    ring buffer started

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,6 +1,12 @@
 class RingBuffer:
+    #Constuctor initilize and define buffer
     def __init__(self, capacity):
-        pass
+        #create empty array to hold list
+        self.buffer = []
+        #set capacity
+        self.capacity = capacity
+        #define id and set as 0 
+        self.id = 0
 
     def append(self, item):
         pass
